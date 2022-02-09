3164d1c50b3da572385e7e972439a77ea88d8213
Jayavarshini Ganesh Kumar <98834011+Jayavarshini-G@users.noreply.github.com>
2022-02-09 23:21:59 +0530

    Create phonebook.h

---
+++ 3_Implementation/inc/phonebook.h
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <conio.h>
+#include <direct.h>
+#include <string.h>
+
+void menu(void);
+void password(void);
+void name(void);
+void Find(void);
+void list(void);
+void modify(void);
+void deleted(void);
+void exitfun(void);
