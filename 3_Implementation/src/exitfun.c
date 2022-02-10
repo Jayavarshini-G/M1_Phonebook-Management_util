a099ef2a7f84b61a438e6c0d522b3332a412a0db
Jayavarshini Ganesh Kumar <98834011+Jayavarshini-G@users.noreply.github.com>
2022-02-11 00:18:14 +0530

    Create exitfun.c

---
+++ 3_Implementation/src/exitfun.c
@@ -0,0 +1,14 @@
+#include"phonebook.h"
+#include <stdio.h>
+#include <conio.h>
+#include<ctype.h>
+#include <windows.h>
+#include <direct.h>
+#include <stdlib.h>
+void exitfun(){
+	system("cls");
+
+	printf("THANK YOU");
+	system("cls");
+    system("color 7");
+}
