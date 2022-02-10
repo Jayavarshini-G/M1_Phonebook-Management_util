f90d983a619b354d630dbaa279863e4756a31d81
Jayavarshini Ganesh Kumar <98834011+Jayavarshini-G@users.noreply.github.com>
2022-02-10 20:18:48 +0530

    Create phonebook.h

---
+++ 3_Implementation/inc/phonebook.h
@@ -0,0 +1,13 @@
+#ifndef PHONEBOOK_H_INCLUDED
+#define PHONEBOOK_H_INCLUDED
+
+void menu(void);
+void password(void);
+void name(void);
+void Find(void);
+void list(void);
+void modify(void);
+void deleted(void);
+void exitfun(void);
+
+#endif // PHONEBOOK_H_INCLUDED
