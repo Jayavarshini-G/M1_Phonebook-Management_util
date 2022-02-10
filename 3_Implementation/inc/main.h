daef6a145651bc2d8f7557f09ce6e3f8a3491593
Jayavarshini Ganesh Kumar <98834011+Jayavarshini-G@users.noreply.github.com>
2022-02-10 15:54:48 +0530

    Create main.h

---
+++ 3_Implementation/inc/main.h
@@ -0,0 +1,15 @@
+#ifndef PHONEBOOK_H_INCLUDED
+#define PHONEBOOK_H_INCLUDED
+
+
+
+void menu(void);
+void password(void);
+void name(void);
+void Find(void);
+void list(void);
+void modify(void);
+void deleted(void);
+void exitfun(void);
+
+#endif
