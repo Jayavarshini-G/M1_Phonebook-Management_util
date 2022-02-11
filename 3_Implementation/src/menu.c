0aa95706f3819dffe63e843e85b2532ea4e2f2e9
Jayavarshini Ganesh Kumar <98834011+Jayavarshini-G@users.noreply.github.com>
2022-02-11 19:38:21 +0530

    Update menu.c

--- 3_Implementation/src/menu.c
+++ 3_Implementation/src/menu.c
@@ -11,9 +11,8 @@ void menu(){
 	printf("1.Add New\n");
 	printf("2.Find\n");
 	printf("3.List\n");
-	printf("4.Change\n");
-	printf("5.Delete\n");
-	printf("6.Exit\n");
+	printf("4.Delete\n");
+	printf("5.Exit\n");
 	switch(getch()){
 		case '1':
 			name();
@@ -25,12 +24,9 @@ void menu(){
 			list();
 			break;
 		case '4':
-			modify();
-			break;
-		case '5':
 			deleted();
 			break;
-		case '6':
+		case '5':
 			exitfun();
 			break;
 		default:
